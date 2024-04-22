6cce35ea2f4476898d1085e8d56d3531bc2bdcc3
1slom-media <islombektagayev1@gmail.com>
2024-04-23 02:19:20 +0500

    initial commit

---
+++ assets/js/language.js
@@ -0,0 +1,391 @@
+const translations = {
+  en: {
+    navbar: {
+      0: "Home",
+      1: "Menus",
+      2: "About Us",
+      3: "Contact",
+    },
+    topbar: {
+      adress: "Tashkent region, Zangiata district, Erkin MFI Kahramon",
+    },
+    hero: {
+      title1: `Discover a World of Oriental Flavor with Premin Funchses!`,
+      text1: `Exquisite flavor in every strand of funchosas.`,
+      title2: `Surprise the Taste with GDR-product!`,
+      text2: `Funchoza for real gourmets.`,
+      title3: `Turn Dinner into a Taste Celebration!`,
+      text3: `GDR-product: the best choice for your table.`,
+    },
+    about: {
+      subtitle: "our story",
+      title: "About us",
+      text: "GDR-product company was founded in 2010 and has become one of the leading manufacturers of funchoza in Uzbekistan. Our product is characterized by high quality and impeccable taste, which allows us to take a leading position in the market. We are proud that every strand of our funchoza brings joy and satisfaction to our customers.",
+    },
+    features: {
+      0: "Why Choose Us",
+      1: "Our advantages",
+      Benefit1: "Strict product quality control",
+      Description1:
+        "Guaranteeing the safety and high quality of each package of funchosas.",
+      Benefit2: "Safe and natural product",
+      Description2:
+        "The product is non-GMO and is made in compliance with all production technologies.",
+      Benefit3: "Consistency of quality",
+      Description3:
+        "Our commitment to providing consistently high quality products.",
+      Benefit4: "Professional service and support",
+      Description4:
+        "Our team is ready to provide competent assistance and advice to every customer.",
+    },
+    special: {
+      title: "Our principles and values",
+      text: "Our company emphasizes innovative production techniques in an effort to ensure our products are safe and natural. We are proud of our reputation as one of the country's finest funchoza producers and are committed to continuing to delight our customers with our quality and attention to detail.",
+    },
+    testi: {
+      testiText:
+        "Welcome to a world of true flavor and quality with GDR-product: where every strand of Premium fuchoses is an expression of care and attention.",
+    },
+    form: {
+      title: "Contact Us",
+      btnText: "Submit",
+    },
+    socials: {
+      facebook: "FACEBOOK",
+      instagram: "INSTAGRAM",
+      telegram: "Telegram",
+      map: "GOOGLE MAP",
+    },
+    footer: {
+      footerTitle: "Get News & Offers",
+      btn: "Subscribe",
+    },
+  },
+  ru: {
+    navbar: {
+      0: "Дом",
+      1: "Меню",
+      2: "О нас",
+      3: "Контакт",
+    },
+    topbar: {
+      adress: "Ташкентская обл., Зангиатайский район, Эркин МФИ Кахрамон",
+    },
+    hero: {
+      title1: `Откройте Мир Восточного Вкуса с Premin Funchses!`,
+      text1: `Изысканный вкус в каждой нитке фунчозы.`,
+      title2: `Удивляйте Вкусом с GDR-product!`,
+      text2: `Фунчоза для настоящих гурманов.`,
+      title3: `Превратите Обед в Праздник Вкуса!`,
+      text3: `GDR-product: лучший выбор для вашего стола.`,
+    },
+    about: {
+      subtitle: "Наша история",
+      title: "О Нас",
+      text: "Компания GDR-product была основана в 2010 году и стала одним из ведущих производителей фунчозы в Узбекистане. Наш продукт отличается высоким качеством и безупречным вкусом, что позволяет нам занимать лидирующие позиции на рынке. Мы гордимся тем, что каждая нить нашей фунчозы приносит радость и удовлетворение нашим клиентам.",
+    },
+    features: {
+      0: "ПОЧЕМУ ВЫБРАЛИ НАС ",
+      1: "Наши преимущества",
+      Benefit1: "Строгий контроль качества продукции",
+      Description1:
+        "Гарантия безопасности и высокого качества каждой упаковки фунчозы.",
+      Benefit2: "Безопасность и натуральность продукта",
+      Description2:
+        "Продукт не содержит ГМО и изготавливается с соблюдением всех технологий производства.",
+      Benefit3: "Постоянство качества",
+      Description3:
+        "Наше обязательство обеспечивать постоянно высокое качество продукции.",
+      Benefit4: "Профессиональное обслуживание и поддержка",
+      Description4:
+        "Наша команда готова предоставить компетентную помощь и консультации каждому клиенту.",
+    },
+    special: {
+      title: "Наши принципы и ценности",
+      text: "Наша компания уделяет особое внимание инновационным технологиям производства, стремясь обеспечить безопасность и натуральность нашей продукции. Мы гордимся нашей репутацией одного из лучших производителей фунчозы в стране и готовы продолжать радовать наших клиентов своим качеством и вниманием к деталям.",
+    },
+    testi: {
+      testiText:
+        "Приветствуем в мире истинного вкуса и качества с GDR-product: где каждая нить Premium fuchoses – это проявление заботы и внимания.",
+    },
+    form: {
+      title: "Связаться с нами",
+      btnText: "Отправить",
+    },
+    socials: {
+      facebook: "ФЕЙСБУК",
+      instagram: "ИНСТАГРАММ",
+      telegram: "Телеграмм",
+      map: "КАРТА GOOGLE",
+    },
+    footer: {
+      footerTitle: "Получайте новости и предложения",
+      btn: "Подписаться",
+    },
+  },
+  uz: {
+    navbar: {
+      0: "Uy",
+      1: "Menyular",
+      2: "Biz haqimizda",
+      3: "Aloqa",
+    },
+    topbar: {
+      adress: "Toshkent viloyati, Zangiota tumani, Erkin MFI Qaxramon",
+    },
+    hero: {
+      title1: `Premin Funchses bilan Sharq ta'mi dunyosini oching!`,
+      text1: `Funchozaning har bir ipida ajoyib ta'm.`,
+      title2: `GDR-product ta'mi bilan hayratda qoldiring!`,
+      text2: `Haqiqiy tam shinavandalari uchun funchoza.`,
+      title3: `Tushlikni lazzat bayramiga aylantiring!`,
+      text3: `GDR-product: dasturxon uchun eng yaxshi tanlov.`,
+    },
+    about: {
+      subtitle: "bizning hikoyamiz",
+      title: "Biz Haqimizda",
+      text: "GDR-product kompaniyasi 2010 yilda tashkil etilgan va O'zbekistonda funchoza ishlab chiqaruvchi yetakchi kompaniyalardan biriga aylangan. Bizning mahsulotimiz yuqori sifatli va benuqson ta'mga ega, bu bizga bozorda etakchi o'rinni egallashga imkon beradi. Biz funchozamizning har bir ipi mijozlarimizga quvonch va mamnuniyat keltirishidan faxrlanamiz.",
+    },
+    features: {
+      0: "Nega aynan biz",
+      1: "Afzalliklarimiz",
+      Benefit1: "mahsulot sifatini qat'iy nazorat qilish",
+      Description1:
+        "har bir funchoza paketining xavfsizligi va yuqori sifati kafolati.",
+      Benefit2: "mahsulotning xavfsizligi va tabiiyligi",
+      Description2:
+        "mahsulot GMO bo'lmagan va barcha ishlab chiqarish texnologiyalariga muvofiq ishlab chiqariladi.",
+      Benefit3: "sifat barqarorligi",
+      Description3:
+        "doimiy ravishda yuqori sifatli mahsulotlarni taqdim etish majburiyatimiz.",
+      Benefit4: "professional xizmat va qo'llab-quvvatlash",
+      Description4:
+        "bizning jamoamiz har bir mijozga malakali yordam va maslahat berishga tayyor.",
+    },
+    special: {
+      title: "Bizning printsiplarimiz va qadriyatlarimiz",
+      text: "Kompaniyamiz mahsulotlarimizning xavfsizligi va tabiiyligini taʼminlashga intilib, innovatsion ishlab chiqarish texnologiyalariga alohida eʼtibor beradi. Biz mamlakatdagi eng yaxshi funchoza ishlab chiqaruvchilardan biri sifatidagi obro‘  - eʼtiborimiz bilan faxrlanamiz va sifatimiz va tafsilotlarga eʼtiborimiz bilan mijozlarimizni xursand qilishni davom ettirishga tayyormiz.",
+    },
+    testi: {
+      testiText:
+        "GDR-product bilan haqiqiy ta'm va sifat dunyosiga xush kelibsiz: bu erda har bir Premium fuchoses ipi g'amxo'rlik va e'tiborning namoyonidir.",
+    },
+    form: {
+      title: "Biz bilan bog'lanish",
+      btnText: "Yuborish",
+    },
+    socials: {
+      facebook: "FACEBOOK",
+      instagram: "INSTAGRAM",
+      telegram: "Telegram",
+      map: "GOOGLE MAP",
+    },
+    footer: {
+      footerTitle: "Yangiliklar va takliflarni oling",
+      btn: "Obuna bo'lish",
+    },
+  },
+};
+
+const languageSelectTop = document.querySelector(".navbar-language");
+
+languageSelectTop.addEventListener("change", (e) => {
+  setLanguage(e.target.value);
+});
+// navbar
+const link = document.querySelectorAll(".navbar-link>span");
+// topbar
+const adress = document.querySelectorAll(".topbar-adress");
+// hero
+const sliderTitle1 = document.getElementById("hero-slider-1");
+const sliderText1 = document.getElementById("hero-text-1");
+const sliderTitle2 = document.getElementById("hero-slider-2");
+const sliderText2 = document.getElementById("hero-text-2");
+const sliderTitle3 = document.getElementById("hero-slider-3");
+const sliderText3 = document.getElementById("hero-text-3");
+// about
+const aboutSubTitle = document.querySelector(".about-subtitle");
+const aboutTitle = document.querySelector(".about-title");
+const aboutText = document.querySelector(".about-text");
+// features
+const featuresTitle = document.querySelector(".features-title");
+const featuresText = document.querySelector(".features-text");
+const cardTitle = document.querySelectorAll(".features-card-title");
+const cardText = document.querySelectorAll(".features-card-text");
+// special
+const specialTitle = document.querySelector(".special-title");
+const specialText = document.querySelector(".special-text");
+// testi
+const testiText = document.querySelector(".testi-text");
+// form
+const formTitle = document.querySelector(".reserv-form-title");
+const formBtnText = document.querySelectorAll(".form-btn-text");
+// footer
+const footerLinkes = document.querySelectorAll(".footer-link");
+const footerTitle = document.querySelector(".footer-form");
+const footerBtn = document.querySelectorAll(".footer-form-btn");
+
+const setLanguage = (lang) => {
+  if (lang === "en") {
+    // hero
+    sliderTitle1.innerText = translations.en.hero.title1;
+    sliderText1.innerText = translations.en.hero.text1;
+    sliderTitle2.innerText = translations.en.hero.title2;
+    sliderText2.innerText = translations.en.hero.text2;
+    sliderTitle3.innerText = translations.en.hero.title3;
+    sliderText3.innerText = translations.en.hero.text3;
+    // navbar
+    link[0].innerText = translations.en.navbar[0];
+    link[1].innerText = translations.en.navbar[1];
+    link[2].innerText = translations.en.navbar[2];
+    link[3].innerText = translations.en.navbar[3];
+    // topbar
+    adress[0].innerText = translations.en.topbar.adress;
+    adress[1].innerText = translations.en.topbar.adress;
+    adress[2].innerText = translations.en.topbar.adress;
+    // about
+    aboutSubTitle.innerText = translations.en.about.subtitle;
+    aboutTitle.innerText = translations.en.about.title;
+    aboutText.innerText = translations.en.about.text;
+    // features
+    featuresTitle.innerText = translations.en.features[1];
+    featuresText.innerText = translations.en.features[0];
+    cardTitle[0].innerText = translations.en.features.Benefit1;
+    cardTitle[1].innerText = translations.en.features.Benefit2;
+    cardTitle[2].innerText = translations.en.features.Benefit3;
+    cardTitle[3].innerText = translations.en.features.Benefit4;
+    cardText[0].innerText = translations.en.features.Description1;
+    cardText[1].innerText = translations.en.features.Description2;
+    cardText[2].innerText = translations.en.features.Description3;
+    cardText[3].innerText = translations.en.features.Description4;
+    // special
+    specialTitle.innerText = translations.en.special.title;
+    specialText.innerText = translations.en.special.text;
+    // testi
+    testiText.innerText = translations.en.testi.testiText;
+    // form
+    formTitle.innerText = translations.en.form.title;
+    formBtnText[0].innerText = translations.en.form.btnText;
+    formBtnText[1].innerText = translations.en.form.btnText;
+    // footer
+    footerLinkes[0].innerText = translations.en.navbar[0];
+    footerLinkes[1].innerText = translations.en.navbar[1];
+    footerLinkes[2].innerText = translations.en.navbar[2];
+    footerLinkes[3].innerText = translations.en.navbar[3];
+    footerLinkes[4].innerText = translations.en.socials.facebook;
+    footerLinkes[5].innerText = translations.en.socials.instagram;
+    footerLinkes[6].innerText = translations.en.socials.telegram;
+    footerLinkes[7].innerText = translations.en.socials.map;
+    footerTitle.innerText = translations.en.footer.footerTitle;
+    footerBtn[0].innerText = translations.en.footer.btn;
+    footerBtn[1].innerText = translations.en.footer.btn;
+  }
+  if (lang === "ru") {
+    // hero
+    sliderTitle1.innerText = translations.ru.hero.title1;
+    sliderText1.innerText = translations.ru.hero.text1;
+    sliderTitle2.innerText = translations.ru.hero.title2;
+    sliderText2.innerText = translations.ru.hero.text2;
+    sliderTitle3.innerText = translations.ru.hero.title3;
+    sliderText3.innerText = translations.ru.hero.text3;
+    // navbar
+    link[0].innerText = translations.ru.navbar[0];
+    link[1].innerText = translations.ru.navbar[1];
+    link[2].innerText = translations.ru.navbar[2];
+    link[3].innerText = translations.ru.navbar[3];
+    // topbar
+    adress[0].innerText = translations.ru.topbar.adress;
+    adress[1].innerText = translations.ru.topbar.adress;
+    adress[2].innerText = translations.ru.topbar.adress;
+    // about
+    aboutSubTitle.innerText = translations.ru.about.subtitle;
+    aboutTitle.innerText = translations.ru.about.title;
+    aboutText.innerText = translations.ru.about.text;
+    // features
+    featuresTitle.innerText = translations.ru.features[1];
+    featuresText.innerText = translations.ru.features[0];
+    cardTitle[0].innerText = translations.ru.features.Benefit1;
+    cardTitle[1].innerText = translations.ru.features.Benefit2;
+    cardTitle[2].innerText = translations.ru.features.Benefit3;
+    cardTitle[3].innerText = translations.ru.features.Benefit4;
+    cardText[0].innerText = translations.ru.features.Description1;
+    cardText[1].innerText = translations.ru.features.Description2;
+    cardText[2].innerText = translations.ru.features.Description3;
+    cardText[3].innerText = translations.ru.features.Description4;
+    // special
+    specialTitle.innerText = translations.ru.special.title;
+    specialText.innerText = translations.ru.special.text;
+    // testi
+    testiText.innerText = translations.ru.testi.testiText;
+    // form
+    formTitle.innerText = translations.ru.form.title;
+    formBtnText[0].innerText = translations.ru.form.btnText;
+    formBtnText[1].innerText = translations.ru.form.btnText;
+    // footer
+    footerLinkes[0].innerText = translations.ru.navbar[0];
+    footerLinkes[1].innerText = translations.ru.navbar[1];
+    footerLinkes[2].innerText = translations.ru.navbar[2];
+    footerLinkes[3].innerText = translations.ru.navbar[3];
+    footerLinkes[4].innerText = translations.ru.socials.facebook;
+    footerLinkes[5].innerText = translations.ru.socials.instagram;
+    footerLinkes[6].innerText = translations.ru.socials.telegram;
+    footerLinkes[7].innerText = translations.ru.socials.map;
+    footerTitle.innerText = translations.ru.footer.footerTitle;
+    footerBtn[0].innerText = translations.ru.footer.btn;
+    footerBtn[1].innerText = translations.ru.footer.btn;
+  }
+  if (lang === "uz") {
+    // hero
+    sliderTitle1.innerText = translations.uz.hero.title1;
+    sliderText1.innerText = translations.uz.hero.text1;
+    sliderTitle2.innerText = translations.uz.hero.title2;
+    sliderText2.innerText = translations.uz.hero.text2;
+    sliderTitle3.innerText = translations.uz.hero.title3;
+    sliderText3.innerText = translations.uz.hero.text3;
+    // navbar
+    link[0].innerText = translations.uz.navbar[0];
+    link[1].innerText = translations.uz.navbar[1];
+    link[2].innerText = translations.uz.navbar[2];
+    link[3].innerText = translations.uz.navbar[3];
+    // topbar
+    adress[0].innerText = translations.uz.topbar.adress;
+    adress[1].innerText = translations.uz.topbar.adress;
+    adress[2].innerText = translations.uz.topbar.adress;
+    // about
+    aboutSubTitle.innerText = translations.uz.about.subtitle;
+    aboutTitle.innerText = translations.uz.about.title;
+    aboutText.innerText = translations.uz.about.text;
+    // features
+    featuresTitle.innerText = translations.uz.features[1];
+    featuresText.innerText = translations.uz.features[0];
+    cardTitle[0].innerText = translations.uz.features.Benefit1;
+    cardTitle[1].innerText = translations.uz.features.Benefit2;
+    cardTitle[2].innerText = translations.uz.features.Benefit3;
+    cardTitle[3].innerText = translations.uz.features.Benefit4;
+    cardText[0].innerText = translations.uz.features.Description1;
+    cardText[1].innerText = translations.uz.features.Description2;
+    cardText[2].innerText = translations.uz.features.Description3;
+    cardText[3].innerText = translations.uz.features.Description4;
+    // special
+    specialTitle.innerText = translations.uz.special.title;
+    specialText.innerText = translations.uz.special.text;
+    // testi
+    testiText.innerText = translations.uz.testi.testiText;
+    // form
+    formTitle.innerText = translations.uz.form.title;
+    formBtnText[0].innerText = translations.uz.form.btnText;
+    formBtnText[1].innerText = translations.uz.form.btnText;
+    // footer
+    footerLinkes[0].innerText = translations.uz.navbar[0];
+    footerLinkes[1].innerText = translations.uz.navbar[1];
+    footerLinkes[2].innerText = translations.uz.navbar[2];
+    footerLinkes[3].innerText = translations.uz.navbar[3];
+    footerLinkes[4].innerText = translations.uz.socials.facebook;
+    footerLinkes[5].innerText = translations.uz.socials.instagram;
+    footerLinkes[6].innerText = translations.uz.socials.telegram;
+    footerLinkes[7].innerText = translations.uz.socials.map;
+    footerTitle.innerText = translations.uz.footer.footerTitle;
+    footerBtn[0].innerText = translations.uz.footer.btn;
+    footerBtn[1].innerText = translations.uz.footer.btn;
+  }
+};
